36cdf1cfda261ad40d09516bb91ed8f41643f2a8
nora-shirokuma <myself@nora-shirokuma.com>
2024-05-10 17:55:23 +0900

    メソッド名オブジェクトを作成。

---
+++ src/Domain/Method/MethodName.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NoraShirokuma\ClassGeneratorPhp\Domain\Method;
+
+class MethodName
+{
+    private ?string $value;
+
+    public function __construct(?string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+}
